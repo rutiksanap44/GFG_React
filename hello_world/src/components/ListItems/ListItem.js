be77c04ebe3e4c3804a3abad341a5d072d7155dd
rutiksanap44 <99959986+rutiksanap44@users.noreply.github.com>
2024-01-24 21:13:24 -0500

    conditional rendering feature added

--- hello_world/src/components/ListItems/ListItem.js
+++ hello_world/src/components/ListItems/ListItem.js
@@ -1,41 +1,59 @@
-import AddtoCartIcon from '../../assets/icons/cart.svg'
-import { useState } from 'react';
+import AddtoCartIcon from "../../assets/icons/cart.svg";
+import { useState } from "react";
 const ListItem = ({ data }) => {
+  const [counter, setCounter] = useState(0);
 
-    const [counter, setCounter] = useState(1)
-
-    const increaseCounter = () => {
-        setCounter(counter + 1)
+  const increaseCounter = () => {
+    setCounter(counter + 1);
+  };
+  const decreaseCounter = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
     }
-    const decreaseCounter = () => {
-        if (counter > 0) {
-            setCounter(counter - 1)
-        }
-    }
-    return (
-        <div className={'item-card'}>
-            <img className={'img-fluid'} src={data.thumbnail} alt="Cart here" />
-            <div className={'item-card__information'}>
-                <div className={'price_div'}>
-                    <div><span>₹{data.price}</span></div>
-                    <strike>{data.discountedPrice}</strike>
-                </div>
-                <div className={'title'}>
-                    <h2>{data.title}</h2>
-                </div>
-            </div>
-            <span className='cart-message'>{ }</span>
-            <button className={'cart-add'}>
-                <span>Add to Cart</span>
-                <img src={AddtoCartIcon} alt="cart here" />
-            </button>
-            <div className='cart-addon'>
-                <button onClick={() => { decreaseCounter() }}><span>-</span></button>
-                <span className='counter'>{counter}</span>
-                <button onClick={() => { increaseCounter() }}><span>+</span></button>
-            </div>
+  };
+  return (
+    <div className={"item-card"}>
+      <img className={"img-fluid"} src={data.thumbnail} alt="Cart here" />
+      <div className={"item-card__information"}>
+        <div className={"price_div"}>
+          <div>
+            <span>₹{data.price}</span>
+          </div>
+          <strike>{data.discountedPrice}</strike>
         </div>
-    )
+        <div className={"title"}>
+          <h2>{data.title}</h2>
+        </div>
+      </div>
+      <span className="cart-message">{}</span>
+      {counter === 0 ? (
+        <div>
+          <button onClick={increaseCounter} className={"cart-add"}>
+            <span>Add to Cart</span>
+            <img src={AddtoCartIcon} alt="cart here" />
+          </button>
+        </div>
+      ) : (
+        <div className="cart-addon">
+          <button
+            onClick={() => {
+              decreaseCounter();
+            }}
+          >
+            <span>-</span>
+          </button>
+          <span className="counter">{counter}</span>
+          <button
+            onClick={() => {
+              increaseCounter();
+            }}
+          >
+            <span>+</span>
+          </button>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ListItem;
--- hello_world/src/components/Products/Products.js
+++ hello_world/src/components/Products/Products.js
@@ -1,5 +1,5 @@
-import ListItem from '../ListItems/ListItem'
-import { useState } from 'react'
+import ListItem from "../ListItems/ListItem";
+import { useState } from "react";
 
 // const items = [
 //     {
@@ -19,64 +19,81 @@ import { useState } from 'react'
 //     }
 // ]
 
-
 const Products = () => {
+  const [item, setItem] = useState([
+    {
+      id: 0,
+      title: "Title of Product 1",
+      price: 400,
+      discountedPrice: 340,
+      thumbnail: "/assets/random.jpg",
+    },
+    {
+      id: 0,
+      title: "Title of Product 2",
+      price: 400,
+      discountedPrice: 340,
+      thumbnail: "/assets/random.jpg",
+    }
+  ]);
+  // const handleTitle = (event) => {
+  //     setItem({
+  //         ...item,
+  //         title:event.target.value
+  //     })
+  // }
+  // const handlePrice = (event) => {
+  //     setItem({
+  //         ...item,
+  //         price:event.target.value
+  //     })
+  //     }
+  // const handleDiscountedPrice = (event) => {
+  //     setItem({
+  //         ...item,
+  //         [event.target.name]:event.target.value,
+  //     })
+  // }
 
-    const[item, setItem] = useState({
-            id: 0,
-            title: "Title of Product 1",
-            price: 400,
-            discountedPrice: 340,
-            thumbnail: "/assets/random.jpg"
-    
-    })
-    // const handleTitle = (event) => {
-    //     setItem({
-    //         ...item,
-    //         title:event.target.value
-    //     })        
-    // }
-    // const handlePrice = (event) => {
-    //     setItem({
-    //         ...item,
-    //         price:event.target.value
-    //     })
-    //     }
-    // const handleDiscountedPrice = (event) => {
-    //     setItem({
-    //         ...item,
-    //         [event.target.name]:event.target.value,
-    //     })
-    // } 
+  const handleInput = (event) => {
+    setItem({
+      ...item,
+      [event.target.name]: event.target.value,
+    });
+  };
 
-    const handleInput = event => {
-        setItem({
-            ...item,
-            [event.target.name]:event.target.value
+  const handleButton = (event) => {};
+  return (
+    <div className="product-list">
+      {/* <div><input onChange={addProduct}></input></div> */}
+      {
+        item.map(elements => {
+            return (<ListItem data={elements}/>)
         })
-    }
-    
-    const handleButton = (event) => {
-        
-    }
-    return (
-        <div className='product-list'>
-          { /* <div><input onChange={addProduct}></input></div> */}
-          <ListItem data={item}></ListItem>
-          <div>
-          <form>
-          <label>Title</label><br/>
-          <input name='title' value={item.title} onChange={handleInput}></input><br/>
-          <label>Price</label><br/>
-          <input name='price' value={item.price} onChange={handleInput}></input><br/>
-          <label>Discounted Price</label><br/>
-          <input name='discountedPrice' value={item.discountedPrice} onChange={handleInput}></input><br/>
+      }
+      <div>
+        <form>
+          <label>Title</label>
+          <br />
+          <input name="title" value={item.title} onChange={handleInput}></input>
+          <br />
+          <label>Price</label>
+          <br />
+          <input name="price" value={item.price} onChange={handleInput}></input>
+          <br />
+          <label>Discounted Price</label>
+          <br />
+          <input
+            name="discountedPrice"
+            value={item.discountedPrice}
+            onChange={handleInput}
+          ></input>
+          <br />
           <button onClick={handleButton}>Submit</button>
-          </form>
-
-          </div>
-        </div>
-    )
-}
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Products;
